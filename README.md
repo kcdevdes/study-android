577a6ffb335de1928b50789c92481ba259e3f5c7
kcdevdes <kcdevdes@gmail.com>
2022-04-05 16:12:16 -0400

    added a new module that demonstrates how to use EventListeners, TouchEvents and Chronometer View Widget.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hi there! This repository stores what I've learn about Android Programming with Kotlin lang
+
+I like Android OS. I like Kotlin language as well. Needless to say, I need to learn Android then :)
+
+* Apr 03, 2022 : RelativeLayout & LinearLayout & GridLayout
---
+++ ch8_event/src/main/java/com/kvicwhite/application/ch8_event/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.kvicwhite.application.ch8_event
+
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kvicwhite.application.ch8_event.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var initTime = 0L
+    var pauseTime = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
+
+            binding.startButton.isEnabled = false
+            binding.stopButton.isEnabled = true
+            binding.resetButton.isEnabled = true
+        }
+
+        binding.stopButton.setOnClickListener {
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.startButton.isEnabled = true
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = true
+        }
+
+        binding.resetButton.setOnClickListener {
+            pauseTime = 0L
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.startButton.isEnabled = true
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = false
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - initTime > 3000) {
+                Toast.makeText(this, "Press Back Button once more to terminate!", Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true // ignores terminating this program
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
